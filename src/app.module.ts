759921570fbd930f80feee0b5ac7f363799fe4b1
João Vitor Ribeiro <134459899+joaoribeiro74@users.noreply.github.com>
2025-10-06 18:09:28 -0300

    feat(user): implement CRUD with Prisma and MySQL integration (#2)

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,14 @@ import { AppService } from './app.service';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
 import { ReviewsModule } from './reviews/reviews.module';
-import { UsersModule } from './users/users.module';
 import { AuthMiddleware } from './auth/auth.middleware';
 import { ErrorsController } from './errors/errors.controller';
 import { ErrorsModule } from './errors/errors.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [SharedModule, CoreModule, ReviewsModule, UsersModule, ErrorsModule],
+  imports: [SharedModule, CoreModule, ReviewsModule, ErrorsModule, PrismaModule, UserModule],
   controllers: [AppController, ErrorsController],
   providers: [AppService],
 })
---
+++ src/user/dto/user.dto.ts
@@ -0,0 +1,13 @@
+import { IsString, IsEmail, IsInt, IsOptional } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsOptional()
+  @IsInt()
+  age?: number;
+}
